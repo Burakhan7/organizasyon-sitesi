7c020ce31c9a777e790bd72990f25295867bd76d
Burakhan <burakhangultoplar@gmail.com>
2026-07-10 10:01:16 +0300

    Admin tablosu oluşturuldu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using OrganizasyonSitesi.Data;
 using OrganizasyonSitesi.Services;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,23 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;  // sembol zorunlu olmasın
+    options.Password.RequireUppercase = true;
+    options.Lockout.MaxFailedAccessAttempts = 5;      // 5 yanlış deneme = kilit
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+})
+.AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Admin/Giris";        // korumalı sayfaya girmeye çalışan buraya yönlenir
+    options.AccessDeniedPath = "/Admin/Giris";
+    options.ExpireTimeSpan = TimeSpan.FromHours(8);
+});
 
 builder.Services.AddScoped<IHizmetService, HizmetService>();
 var app = builder.Build();
@@ -31,6 +49,8 @@ app.UseStatusCodePagesWithReExecute("/Home/BulunamayanSayfa");
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -40,5 +60,23 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// İlk açılışta admin kullanıcısını oluştur (yoksa)
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    var adminEmail = builder.Configuration["AdminUser:Email"];
+    var adminSifre = builder.Configuration["AdminUser:Sifre"];
+
+    if (!string.IsNullOrEmpty(adminEmail) && !string.IsNullOrEmpty(adminSifre))
+    {
+        var mevcut = await userManager.FindByEmailAsync(adminEmail);
+        if (mevcut == null)
+        {
+            var admin = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+            await userManager.CreateAsync(admin, adminSifre);
+        }
+    }
+}
 
 app.Run();
